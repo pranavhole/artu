1a274501c2db48b795ccd9354d40e2be08395780
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2023-12-06 13:54:00 +0530

    sorry

--- src/Components/notificationBlx.js
+++ src/Components/notificationBlx.js
@@ -1,11 +1,33 @@
-import React from 'react'
+// Carousel.js
+import React, { useEffect,useRef } from 'react';
+import  './Rotating.css'; // Import the CSS module
 
-function NotificationBlx() {
+const NotificationBlx = () => {
+  const listRef = useRef(null);
+
+  useEffect(() => {
+    const list = listRef.current;
+    const listContent = Array.from(list.children);
+
+    listContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute('aria-hidden', true);
+      list.appendChild(duplicatedItem);
+    });
+  }, []);
   return (
-    <div>
-      <h1>NotificationBlx</h1>
+    <div id="container" data-animated>
+      <ul ref={listRef} id="list">
+        <li>
+          <div>
+            hello
+          </div>
+        </li>
+        <li>Second</li>
+        <li>Third</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default NotificationBlx
+export default NotificationBlx;
